231f2f44cfae5f039c49900bb0a0ec4c24021d49
Reed Trevelyan <reedtrevelyan@gmail.com>
2022-08-16 22:43:34 -0400

    build site

--- src/App.js
+++ src/App.js
@@ -28,15 +28,21 @@ ChartJS.register(
 function App() {
   const [tempData, setTempData] = useState(null);
   const [humidityData, setHumidityData] = useState(null);
+  const [vocData, setVocData] = useState(null);
+  const [pressureData, setPressureData] = useState(null);
 
   const getSensorData = async () => {
-    const { data } = await axios.get('http://quillsecure.com/api/dashboard/stats');
-    const filteredData = data.reduce((prev, curr, i) => (i % 15 === 0 ? [...prev, curr] : prev), []);
+    const { data } = await axios.get('http://www.quillsecure.com/api/dashboard/stats');
+    const filteredData = data.reduce((prev, curr, i) => (i % 30 === 0 ? [...prev, curr] : prev), []);
     const td = filteredData.map((d) => ({ x: d.unixTS * 1000, y: d.temperatureF }));
     const hd = filteredData.map((d) => ({ x: d.unixTS * 1000, y: d.humidity }));
+    const pd = filteredData.map((d) => ({ x: d.unixTS * 1000, y: d.pressure }));
+    const vd = filteredData.map((d) => ({ x: d.unixTS * 1000, y: d.vocIndex }));
 
     setTempData(td);
     setHumidityData(hd);
+    setVocData(vd);
+    setPressureData(pd);
   };
 
   useEffect(() => {
@@ -51,15 +57,32 @@ function App() {
       tension: 0.1,
     },
     {
-      label: 'Humidity (F)',
+      label: 'Humidity',
       data: humidityData,
       borderColor: 'rgb(250, 88, 54)',
       tension: 0.1,
       yAxisID: 'y1',
+    },
+    {
+      label: 'VOC Index',
+      data: vocData,
+      borderColor: 'rgb(54, 250, 64)',
+      tension: 0.1,
+      yAxisID: 'y2',
+    },
+    {
+      label: 'Pressure (mbar)',
+      data: pressureData,
+      borderColor: 'rgb(204, 54, 250)',
+      tension: 0.1,
+      yAxisID: 'y3',
     }],
   };
 
   const chartOptions = {
+    animation: {
+      duration: 0,
+    },
     scales: {
       x: {
         type: 'time',
@@ -71,6 +94,9 @@ function App() {
         type: 'linear',
         display: true,
         position: 'left',
+        ticks: {
+          color: 'rgb(75, 192, 192)',
+        },
       },
       y1: {
         type: 'linear',
@@ -80,6 +106,33 @@ function App() {
         // grid line settings
         grid: {
           drawOnChartArea: false, // only want the grid lines for one axis to show up
+        },
+        ticks: {
+          color: 'rgb(250, 88, 54)',
+        },
+      },
+      y2: {
+        type: 'linear',
+        display: true,
+        position: 'right',
+
+        grid: {
+          drawOnChartArea: false,
+        },
+        ticks: {
+          color: 'rgb(54, 250, 64)',
+        },
+      },
+      y3: {
+        type: 'linear',
+        display: true,
+        position: 'left',
+
+        grid: {
+          drawOnChartArea: false,
+        },
+        ticks: {
+          color: 'rgb(204, 54, 250)',
         },
       },
     },
